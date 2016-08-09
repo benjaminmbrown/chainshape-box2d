9bee355f5714408a4c0c2f18684284d86ce94144
Benjamin M. Brown <benjaminbb@gmail.com>
2016-08-09 00:57:20 -0500

    integrate particle and chainshape into sketch

--- sketch.js
+++ sketch.js
@@ -10,18 +10,15 @@ var b2Vec2 = Box2D.Common.Math.b2Vec2,
     b2DebugDraw = Box2D.Dynamics.b2DebugDraw;
 
 var world;
-var boundaries = [];
-var boxes = [];
+var particles = [];
+var surface = [];
 
 
 
 function setup() {
 	createCanvas(640,360);
-	//var world = new box2d.b2World();
-	world = new b2World(new b2Vec2(0, 10),true);
-	//var world = new b2World( new b2Vec2(0.0, -10.0) );
-	//var world = createWorld();
-	var b = new Box(width/2,30);
+	world = createWorld();
+	surface = new Surface()
 
 }
 
@@ -30,16 +27,19 @@ function draw(){
 	var timeStep = 1.0/30;
 	world.step(timeStep,10,10);//2&3 locs are for velocity and pos iterators
 
-	//boxes get randomly generated
+	
 	if(random(1)<0.2){
-		var b = new Box(width/2,30);
-		boxes.push(b);
+		var b = new Particle(width/2,30,random(4,8));
+		particles.push(b);
 	}
 
-	for(var i = boxex.length-1; i>=0; i--){
-		boxes[i].display();
-		if(boxes[i].done()){
-			boxes.splice(i,1);
+	surface.display()
+
+
+	for(var i = particles.length-1; i>=0; i--){
+		particles[i].display();
+		if(particles[i].done()){
+			particles.splice(i,1);
 		}
 	}
 
